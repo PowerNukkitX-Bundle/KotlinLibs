cce023c4377031b9ace1b6d7cc902c9056917ac7
CoolLoong <1542536763@qq.com>
2024-03-29 22:39:54 +0800

    feat: init

---
+++ src/main/kotlin/com/github/coolloong/Main.kt
@@ -0,0 +1,10 @@
+package com.github.coolloong
+
+import cn.nukkit.plugin.PluginBase
+
+
+class KotlinLibs() : PluginBase() {
+    override fun onLoad() {
+        println("KotlinLibs Load success!")
+    }
+}
